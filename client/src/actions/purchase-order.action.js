4b44075e678704e4004572f090830fa1ef7669ae
Chamika Ravinda <chamikaravinda@gmail.com>
2025-02-02 21:28:24 +0530

    Adding view and delete po

--- client/src/actions/purchase-order.action.js
+++ client/src/actions/purchase-order.action.js
@@ -5,12 +5,18 @@ import {
   dispatchSuccess,
 } from "./notifications.action";
 
-
-
-
-export const getPurchaseOrders = async (startIndex,limit, success) => {
+export const getPurchaseOrders = async (
+  startIndex,
+  limit,
+  filters,
+  success
+) => {
   dispatchStartLoading();
-  await fetch(`/api/purchase-order/get?startIndex=${startIndex}&limit=${limit}}`)
+  await fetch(
+    `/api/purchase-order/get?startIndex=${startIndex}&limit=${limit}${setSearchFilters(
+      filters
+    )}`
+  )
     .then((res) => res.json())
     .then((payload) => {
       if (!payload.success) {
@@ -23,4 +29,38 @@ export const getPurchaseOrders = async (startIndex,limit, success) => {
     .catch((error) => {
       dispatchError(error.message);
     });
+};
+
+
+export const deletePurchaseOrders = async (
+  orderId, success
+) => {
+  dispatchStartLoading();
+  await fetch(
+    `/api/purchase-order/${orderId}`, {
+      method: "DELETE",
+    }
+  )
+    .then((res) => res.json())
+    .then((payload) => {
+      if (!payload.success) {
+        dispatchError(payload.message);
+        return;
+      }
+      success();
+      dispatchSuccess(payload.message);
+    })
+    .catch((error) => {
+      dispatchError(error.message);
+    });
+};
+
+const setSearchFilters = (filters) => {
+  let query = "";
+  Object.keys(filters).forEach((key) => {
+    if (filters[key] !== "") {
+      query += `&${key}=${filters[key]}`;
+    }
+  });
+  return query;
 };
--- client/src/utils/commonConstants.js
+++ client/src/utils/commonConstants.js
@@ -1,10 +1,11 @@
 export const USER_ROLE_ADMIN = "ADMIN";
 export const USER_ROLE_USER = "USER";
 
-export const primary_gradient = "bg-gradient-to-r from-[#92226d] to-slate-600"
-
-export const red_gradient = "bg-gradient-to-r from-[#e60707] to-[#e04b0b]"
-
-export const blue_gradient = "bg-gradient-to-r from-[#0909ed] to-[#098aed]"
-
-export const green_gradient = "bg-gradient-to-r from-[#18c912] to-[#0a7a06]"
+export const primary_gradient = "bg-gradient-to-r from-[#92226d] to-slate-600";
+export const red_gradient = "bg-gradient-to-r from-[#e60707] to-[#e04b0b]";
+export const blue_gradient = "bg-gradient-to-r from-[#0909ed] to-[#098aed]";
+export const green_gradient = "bg-gradient-to-r from-[#18c912] to-[#0a7a06]";
+export const primary_button_gradient =
+  "bg-gradient-to-r from-[#92226d] to-gray-800";
+export const secondary_button_gradient =
+  "bg-gradient-to-r from-slate-600 to-gray-800";
